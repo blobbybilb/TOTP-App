0f9652a885646ac3e5cd95ef83b4d6b40a3fbe15
blobbybilb <blobbybilb@randomblob.studio>
2023-03-04 17:18:48 -0800

    update stuff

--- app/src/lib/components/Config.svelte
+++ app/src/lib/components/Config.svelte
@@ -1,35 +1,35 @@
-<script>
-  let token = localStorage.getItem('remoteToken')
-  let password
-  function save() {}
+<script lang="ts">
+    let token = localStorage.getItem('remoteToken')
+    let password: string
+    function save() {}
 </script>
 
 <main>
-  <div id="main">
-    <h2>Remote Sync</h2>
-    <div class="token">
-      <label for="token">Token</label>
-      <input type="text" id="token" bind:value={token} />
+    <div id="main">
+        <h2>Remote Sync</h2>
+        <div class="token">
+            <label for="token">Token</label>
+            <input type="text" id="token" bind:value={token} />
 
-      <label for="password">Password</label>
-      <input type="password" id="password" bind:value={password} />
+            <label for="password">Password</label>
+            <input type="password" id="password" bind:value={password} />
 
-      <button on:click={save}>Save</button>
+            <button on:click={save}>Save</button>
+        </div>
     </div>
-  </div>
 </main>
 
 <style>
-  #main {
-    border-radius: 20px;
-    background-color: var(--accent-background-color);
-    position: fixed;
-    top: 30%;
-    height: 200px;
-    width: 300px;
-    z-index: 2;
-  }
-  * {
-    display: block;
-  }
+    #main {
+        border-radius: 20px;
+        background-color: var(--accent-background-color);
+        position: fixed;
+        top: 30%;
+        height: 200px;
+        width: 300px;
+        z-index: 2;
+    }
+    * {
+        display: block;
+    }
 </style>
